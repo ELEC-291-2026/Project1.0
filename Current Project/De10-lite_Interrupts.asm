8b1821deac08ad2d27085eddbf0feaefd00a5b97
Owen Anderson <65140720+oander03@users.noreply.github.com>
2026-02-05 13:17:19 -0800

    Add files via upload

---
+++ Current Project/De10-lite_Interrupts.asm
@@ -0,0 +1,7 @@
+; Blinky.asm: blinks LEDR0 of the CV-8052 each second.
+$MODMAX10
+
+org 0000H
+	ljmp myprogram
+
+
---
+++ Current Project/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\pamel\Downloads\DE10Lite_asm_Examples\Current Project\"
+"C:\Program Files\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\pamel\Downloads\DE10Lite_asm_Examples\Current Project\ADC_to_voltage_7seg.asm"
+echo Crosside_Action Set_Hex_File C:\Users\pamel\Downloads\DE10Lite_asm_Examples\Current Project\ADC_to_voltage_7seg.HEX
