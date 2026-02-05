d0da8d559d228e9204a3b644df20ea8c2bebf8fc
Owen Anderson <65140720+oander03@users.noreply.github.com>
2026-02-05 13:40:16 -0800

    Delete DE10Lite directory

--- DE10Lite/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\pamel\Downloads\DE10Lite_asm_Examples\DE10Lite\"
-"C:\Program Files\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\pamel\Downloads\DE10Lite_asm_Examples\DE10Lite\ADC_to_voltage_7seg.asm"
-echo Crosside_Action Set_Hex_File C:\Users\pamel\Downloads\DE10Lite_asm_Examples\DE10Lite\ADC_to_voltage_7seg.HEX
