f8d1022d7f70014c23e8bf5c435f50dbffb90940
Matthieu Moisan <matthieumoisan@bell.net>
2026-02-05 14:34:24 -0800

    Refactor and update comments in De10-lite_project.asm

--- Current Project/De10-lite_project.asm
+++ Current Project/De10-lite_project.asm
@@ -31,6 +31,9 @@ dseg at 0x30
 x:		ds	4
 y:		ds	4
 bcd:	ds	5
+tempHot:ds  5
+tempCold:ds 5
+
 ; Each FSM has its own timer
 FSM1_timer: ds 1
 FSM2_timer: ds 1
@@ -444,6 +447,7 @@ Skip_Count3:
     
     
 END
+
 
 
 
