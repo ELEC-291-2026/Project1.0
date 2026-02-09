fca63f3c0d58d11e46f0be83f26cfe533fcf370f
Owen Anderson <65140720+oander03@users.noreply.github.com>
2026-02-09 11:29:04 -0800

    Delete Current Project/__a51.bat

--- Current Project/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\sinpe\Documents\Project1.0\Current Project\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\sinpe\Documents\Project1.0\Current Project\Read_keypad.asm"
-echo Crosside_Action Set_Hex_File C:\Users\sinpe\Documents\Project1.0\Current Project\Read_keypad.HEX
