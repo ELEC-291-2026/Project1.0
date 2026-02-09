31c258c34fb6f56ae40b9e367375de8647cb90f8
Guanyu <gzhu06@student.ubc.ca>
2026-02-09 12:15:22 -0800

    keypadlib update

--- Current Project/__a51.bat
+++ Current Project/__a51.bat
@@ -1,5 +1,5 @@
 @echo off
 C:
 cd "\Users\sinpe\Documents\Project1.0\Current Project\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\sinpe\Documents\Project1.0\Current Project\Read_keypad.asm"
-echo Crosside_Action Set_Hex_File C:\Users\sinpe\Documents\Project1.0\Current Project\Read_keypad.HEX
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\sinpe\Documents\Project1.0\Current Project\De10-lite_project.asm"
+echo Crosside_Action Set_Hex_File C:\Users\sinpe\Documents\Project1.0\Current Project\De10-lite_project.HEX
