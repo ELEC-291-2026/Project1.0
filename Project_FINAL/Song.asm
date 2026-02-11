fef1101e172dec2b58aaa397e57075d89c674e1a
ttrieu2025 <cattrieutruongminh@gmail.com>
2026-02-10 23:17:44 -0800

    updated song

--- Project_FINAL/Song.asm
+++ Project_FINAL/Song.asm
@@ -1,25 +1,16 @@
-; ISR_example_DE10Lite.asm:
-; a) Increments/decrements a BCD variable every half second using
-;    an ISR for timer 2.  Uses SW0 to decide.  Also 'blinks' LEDR0 every
-;    half a second.
-; b) Generates a 2kHz square wave at pin P1.5 using an ISR for timer 0.
-; c) In the 'main' loop it displays the variable incremented/decremented
-;    using the ISR for timer 2 on the LCD and the 7-segment displays.
-;    Also resets it to zero if the KEY1 pushbutton  is pressed.
-; d) Controls the LCD using general purpose pins P2.3, P2.5, P2.7, P3.1, P3.3, P3.5, P3.7
+; Simple_Rhythm_DE10Lite.asm:
+; Plays C D E F G A B C once (no loop)
+; Press KEY.1 to play again
 ;
 $NOLIST
 $MODMAX10
 $LIST
 
 CLK           EQU 33333333 ; Microcontroller system crystal frequency in Hz
-TIMER0_RATE   EQU 4096     ; 2048Hz squarewave (peak amplitude of CEM-1203 speaker)
-TIMER0_RELOAD EQU ((65536-(CLK/(12*TIMER0_RATE)))) ; The prescaler in the CV-8052 is always 12 unlike the N76E003 where is selectable.
 TIMER2_RATE   EQU 1000     ; 1000Hz, for a timer tick of 1ms
 TIMER2_RELOAD EQU ((65536-(CLK/(12*TIMER2_RATE))))
 
 SOUND_OUT     equ P0.4
-UPDOWN        equ SWA.0
 
 ; Reset vector
 org 0x0000
@@ -49,78 +40,123 @@ org 0x0023
 org 0x002B
 	ljmp Timer2_ISR
 
-; In the 8051 we can define direct access variables starting at location 0x30 up to location 0x7F
+; Variables
 dseg at 0x30
-Count1ms:     ds 2
-
-song_idx:     ds 1   ; which note we�re on
+Count1ms:     ds 2   ; 1ms counter for Timer2
+song_idx:     ds 1   ; which note we're on (0-7)
 note_ms:      ds 2   ; how long current note has played (ms)
-
 tone_rh:      ds 1   ; Timer0 reload high byte for current note
 tone_rl:      ds 1   ; Timer0 reload low byte for current note
+note_dur_l:   ds 1   ; Current note duration low byte
+note_dur_h:   ds 1   ; Current note duration high byte
 
-; In the 8051 we have variables that are 1-bit in size.  We can use the setb, clr, jb, and jnb
-; instructions with these variables.  This is how you define a 1-bit variable:
 bseg
-half_seconds_flag: dbit 1 ; Set to one in the ISR every time 500 ms had passed
+song_playing: dbit 1 ; Flag: is song currently playing?
 
 cseg
-; These 'equ' must match the wiring between the DE10Lite board and the LCD!
-; P0 is in connector JPIO.  Check "CV-8052 Soft Processor in the DE10Lite Board: Getting
-; Started Guide" for the details.
-ELCD_RS equ P3.7
-ELCD_RW equ P3.5
-ELCD_E  equ P3.3
-ELCD_D4 equ P3.1
-ELCD_D5 equ P2.7
-ELCD_D6 equ P2.5
-ELCD_D7 equ P2.3
 
 ; -------- Note reload values (CLK = 33,333,333 Hz, prescaler = 12) --------
 ; Timer0 overflow rate = 2*freq (because we toggle pin each overflow)
 
-R_A4  EQU 0F3ABh   ; 440.00 Hz
-R_B4  EQU 0F504h   ; 493.88 Hz
-R_C5  EQU 0F5A2h   ; 523.25 Hz
-R_D5  EQU 0F6C3h   ; 587.33 Hz
-R_E5  EQU 0F7C5h   ; 659.26 Hz
-R_F5  EQU 0F83Bh   ; 698.46 Hz
-R_G5  EQU 0F914h   ; 783.99 Hz
-
-REST  EQU 00000h   ; special: silence (we�ll stop Timer0)
-
-; Song event format per note:
-;   DW reload_value
-;   DW duration_ms
-;
-; ABC scale: A B C D E F G | G F E D C B A
-ABC_SONG:
-    DW R_A4, 250
-    DW R_B4, 250
-    DW R_C5, 250
-    DW R_D5, 250
-    DW R_E5, 250
-    DW R_F5, 250
-    DW R_G5, 400
-    DW REST, 100
-    DW R_G5, 250
-    DW R_F5, 250
-    DW R_E5, 250
-    DW R_D5, 250
-    DW R_C5, 250
-    DW R_B4, 250
-    DW R_A4, 500
-    DW REST, 300
-
-ABC_SONG_LEN EQU 16    ; number of (reload,duration) pairs above
+R_C5  EQU 0F5A2h   ; 523.25 Hz (Do)
+R_Cs5 EQU 0F63Fh   ; 622.25 Hz (Do Sharp)
+R_D5  EQU 0F6C3h   ; 587.33 Hz (Re)
+R_Ds5 EQU 0F74Dh   ; 739.99 Hz (Dsharp)
+R_E5  EQU 0F7C5h   ; 659.26 Hz (Mi)
+R_F5  EQU 0F83Bh   ; 698.46 Hz (Fa)
+R_Fs5 EQU 0F8ACh   ; 739.99 Hz (Fa Sharp)
+R_G5  EQU 0F914h   ; 783.99 Hz (Sol)
+R_Gs5 EQU 0F97Bh   ; 830.61 Hz (Sol Sharp)
+R_A5  EQU 0F9D6h   ; 
+R_As5 EQU 0FA31h
+R_B5  EQU 0FA82h
+R_C6  EQU 0FAD0h   ; 1046.50 Hz (Do - higher octave)
 
 
-$NOLIST
-$include(LCD_4bit_DE10Lite.inc) ; A library of LCD related functions and utility macros
-$LIST
+; Simple rhythm: C D E F G A B C
+; Each note plays for 400ms
+SIMPLE_RHYTHM:
+    DW R_C5, 400    ; Do
+    DW R_C5, 400    ; Do
+    DW R_G5, 400    ; Sol
+    DW R_G5, 400    ; Sol
+    DW R_A5, 400    ; La
+    DW R_A5, 400    ; La
+    DW R_G5, 600    ; Sol
+    DW R_F5, 400    ; Fa
+    DW R_F5, 400    ; Fa
+    DW R_E5, 400    ; Sol
+    DW R_E5, 400    ; La
+    DW R_D5, 400    ; Si
+    DW R_D5, 400    ; Do (longer final note)
+    DW R_C5, 600    ; Do (longer final note)
+    
 
-;                     1234567890123456    <- This helps determine the location of the counter
-Initial_Message:  db 'BCD_counter: xx ', 0
+SONG_LEN EQU 14    ; number of notes
+
+;---------------------------------;
+; Load current note from song     ;
+; Input: song_idx (0-7)          ;
+;---------------------------------;
+Load_Song_Note:
+    push acc
+    push dph
+    push dpl
+    
+    ; Each note is 4 bytes: 2 for reload, 2 for duration
+    ; Offset = song_idx * 4
+    mov a, song_idx
+    rl a              ; *2
+    rl a              ; *4
+    anl a, #0FCh      ; Ensure it's a multiple of 4
+    
+    mov dptr, #SIMPLE_RHYTHM
+    
+    ; Get reload high byte
+    movc a, @a+dptr
+    mov tone_rh, a
+    
+    ; Get reload low byte
+    mov a, song_idx
+    rl a
+    rl a
+    anl a, #0FCh
+    inc a
+    movc a, @a+dptr
+    mov tone_rl, a
+    
+    ; Get duration low byte
+    mov a, song_idx
+    rl a
+    rl a
+    anl a, #0FCh
+    add a, #2
+    movc a, @a+dptr
+    mov note_dur_h, a
+    
+    ; Get duration high byte
+    mov a, song_idx
+    rl a
+    rl a
+    anl a, #0FCh
+    add a, #3
+    movc a, @a+dptr
+    mov note_dur_l, a
+    
+    ; Reset note timing counter
+    clr a
+    mov note_ms+0, a
+    mov note_ms+1, a
+    
+    ; Load timer and start
+    mov TH0, tone_rh
+    mov TL0, tone_rl
+    setb TR0              ; Start Timer0
+    
+    pop dpl
+    pop dph
+    pop acc
+    ret
 
 ;---------------------------------;
 ; Routine to initialize the ISR   ;
@@ -131,23 +167,20 @@ Timer0_Init:
 	anl a, #0xf0 ; Clear the bits for timer 0
 	orl a, #0x01 ; Configure timer 0 as 16-timer
 	mov TMOD, a
-	mov TH0, #high(TIMER0_RELOAD)
-	mov TL0, #low(TIMER0_RELOAD)
-	; Enable the timer and interrupts
+	
+	; Don't start timer yet - Load_Song_Note will do it
     setb ET0  ; Enable timer 0 interrupt
-    setb TR0  ; Start timer 0
 	ret
 
 ;---------------------------------;
-; ISR for timer 0.  Set to execute;
-; every 1/4096Hz to generate a    ;
-; 2048 Hz square wave at pin P3.7 ;
+; ISR for timer 0.                ;
+; Generates square wave for tone  ;
 ;---------------------------------;
 Timer0_ISR:
-	;clr TF0  ; According to the data sheet this is done for us already.
-	mov TH0, #high(TIMER0_RELOAD) ; Timer 0 doesn't have autoreload in the CV-8052
-	mov TL0, #low(TIMER0_RELOAD)
-	cpl SOUND_OUT ; Connect speaker to P3.7!
+	; Reload timer with current note's value
+	mov TH0, tone_rh
+	mov TL0, tone_rl
+	cpl SOUND_OUT ; Toggle speaker
 	reti
 
 ;---------------------------------;
@@ -161,7 +194,7 @@ Timer2_Init:
 	; Set the reload value
 	mov RCAP2H, #high(TIMER2_RELOAD)
 	mov RCAP2L, #low(TIMER2_RELOAD)
-	; Init One millisecond interrupt counter.  It is a 16-bit variable made with two 8-bit parts
+	; Init One millisecond interrupt counter
 	clr a
 	mov Count1ms+0, a
 	mov Count1ms+1, a
@@ -171,48 +204,49 @@ Timer2_Init:
 	ret
 
 ;---------------------------------;
-; ISR for timer 2                 ;
+; ISR for timer 2 (1ms tick)      ;
+; Handles note timing             ;
 ;---------------------------------;
 Timer2_ISR:
-	clr TF2  ; Timer 2 doesn't clear TF2 automatically. Do it in ISR
-	cpl P1.1 ; To check the interrupt rate with oscilloscope. It must be precisely a 1 ms pulse.
+	clr TF2  ; Timer 2 doesn't clear TF2 automatically
+	cpl P1.1 ; Debug: Check interrupt rate with oscilloscope
 	
-	; The two registers used in the ISR must be saved in the stack
 	push acc
 	push psw
 	
-	; Increment the 16-bit one mili second counter
-	inc Count1ms+0    ; Increment the low 8-bits first
-	mov a, Count1ms+0 ; If the low 8-bits overflow, then increment high 8-bits
-	jnz Inc_Done
-	inc Count1ms+1
-
-Inc_Done:
-	; Check if half second has passed
-	mov a, Count1ms+0
-	cjne a, #low(500), Timer2_ISR_done ; Warning: this instruction changes the carry flag!
-	mov a, Count1ms+1
-	cjne a, #high(500), Timer2_ISR_done
+	; Only process if song is playing
+	jnb song_playing, Timer2_ISR_done
 	
-	; 500 milliseconds have passed.  Set a flag so the main program knows
-	setb half_seconds_flag ; Let the main program know half second had passed
-	; Toggle LEDR0 so it blinks
-	cpl LEDRA.0
-	cpl TR0 ; Enable/disable timer/counter 0. This line creates a beep-silence-beep-silence sound.
-	; Reset to zero the milli-seconds counter, it is a 16-bit variable
-	clr a
-	mov Count1ms+0, a
-	mov Count1ms+1, a
-	; Increment the BCD counter
-	mov a, BCD_counter
-	jb UPDOWN, Timer2_ISR_decrement
-	add a, #0x01
-	sjmp Timer2_ISR_da
-Timer2_ISR_decrement:
-	add a, #0x99 ; Adding the 10-complement of -1 is like subtracting 1.
-Timer2_ISR_da:
-	da a ; Decimal adjust instruction.  Check datasheet for more details!
-	mov BCD_counter, a
+	; Increment note duration counter
+	inc note_ms+0
+	mov a, note_ms+0
+	jnz Check_Note_Duration
+	inc note_ms+1
+	
+Check_Note_Duration:
+	; Check if note duration has elapsed
+	mov a, note_ms+0
+	cjne a, note_dur_l, Timer2_ISR_done
+	mov a, note_ms+1
+	cjne a, note_dur_h, Timer2_ISR_done
+	
+	; Note duration elapsed - advance to next note
+	mov a, song_idx
+	inc a
+	
+	; Check if song is finished
+	cjne a, #SONG_LEN, Next_Note_OK
+	
+	; *** SONG FINISHED - STOP PLAYING ***
+	clr song_playing
+	clr TR0               ; Stop Timer0
+	clr SOUND_OUT         ; Turn off speaker
+	setb LEDRA.0          ; Turn on LED to show song finished
+	sjmp Timer2_ISR_done
+	
+Next_Note_OK:
+	mov song_idx, a
+	lcall Load_Song_Note
 	
 Timer2_ISR_done:
 	pop psw
@@ -227,40 +261,40 @@ Timer2_ISR_done:
 main:
 	; Initialization
     mov SP, #0x7F
+    
+    ; Configure I/O pins
+    mov P0MOD, #00010000b ; P0.4 (SOUND_OUT) is output
+    mov P1MOD, #00000010b ; P1.1 is output (debug)
+    
+    ; Turn off all the LEDs
+    mov LEDRA, #0
+    mov LEDRB, #0
+    
+    ; Initialize timers
     lcall Timer0_Init
     lcall Timer2_Init
-	    mov song_idx, #0
-    lcall Load_Song_Note
-    ; We use the pins of P0 to control the LCD.  Configure as outputs.
-    mov P0MOD, #01111111b ; P0.0 to P0.6 are outputs.  ('1' makes the pin output)
-    ; We use pins P1.5 and P1.1 as outputs also.  Configure accordingly.
-    mov P1MOD, #00100010b ; P1.5 and P1.1 are outputs
-    mov P2MOD, #0xff
-    mov P3MOD, #0xff
-    ; Turn off all the LEDs
-    mov LEDRA, #0 ; LEDRA is bit addressable
-    mov LEDRB, #0 ; LEDRB is NOT bit addresable
+    
+    ; Initialize song (but don't play yet)
+    mov song_idx, #0
+    clr song_playing      ; Wait for button press
+    
     setb EA   ; Enable Global interrupts
 	
-	; After initialization the program stays in this 'forever' loop
+	; Main loop - wait for button to play
 loop:
-	jb KEY.1, loop_a  ; if the KEY1 button is not pressed skip
-	Wait_Milli_Seconds(#50)	; Debounce delay.  This macro is also in 'LCD_4bit_DE1Lite.inc'
-	jb KEY.1, loop_a  ; if the KEY1 button is not pressed skip
-	jnb KEY.1, $		; Wait for button release.  The '$' means: jump to same instruction.
-	; A valid press of the 'BOOT' button has been detected, reset the BCD counter.
-	; But first stop timer 2 and reset the milli-seconds counter, to resync everything.
-	clr TR2 ; Stop timer 2
-	clr a
-	mov Count1ms+0, a
-	mov Count1ms+1, a
-	; Now clear the BCD counter
-	mov BCD_counter, a
-	setb TR2    ; Start timer 2
-	sjmp loop_b ; Display the new value
-loop_a:
-	jnb half_seconds_flag, loop
-loop_b:
-    clr half_seconds_flag ; We clear this flag in the main loop, but it is set in the ISR for timer 2
-    ljmp loop
+	; Press KEY.1 to play the rhythm
+	jb KEY.1, loop
+	jnb KEY.1, $          ; Wait for release
+	
+	; Only play if not already playing
+	jb song_playing, loop
+	
+	; Start playing from beginning
+	clr LEDRA.0           ; Turn off LED
+	mov song_idx, #0
+	lcall Load_Song_Note
+	setb song_playing
+	
+	sjmp loop
+
 END
