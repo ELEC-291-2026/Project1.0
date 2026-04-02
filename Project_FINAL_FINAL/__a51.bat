a1a541bcbd591b3e0647dccfbad6bdd700899bb1
Trieu Truong <cattrieutruongminh@gmail.com>
2026-04-02 11:26:19 -0700

    Delete Project_FINAL_FINAL directory

--- Project_FINAL_FINAL/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\pamel\OneDrive\Desktop\School\CrossIDE\06_Project_1\Current_Project\"
-"C:\Program Files\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\pamel\OneDrive\Desktop\School\CrossIDE\06_Project_1\Current_Project\De10-lite_project.asm"
-echo Crosside_Action Set_Hex_File C:\Users\pamel\OneDrive\Desktop\School\CrossIDE\06_Project_1\Current_Project\De10-lite_project.HEX
